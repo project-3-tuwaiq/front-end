0e77348db1f84da8b8da6eacb30900bb33f0f1bd
IbrahimNAlyami <Ibrahim.nashir.eam.js@tuwaiq.edu.sa>
2021-12-19 16:11:57 +0300

    updatefront

---
+++ project-3/pages/about/index.js
@@ -0,0 +1,9 @@
+import Layout from "../../component/MyLayout"
+
+export default function index() {
+    return (
+            <Layout>
+            <h1> about pge </h1>
+            </Layout>
+    )
+}
---
+++ project-3/pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../component/MyLayout'
+import myHome from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+  
+                <Layout>
+         <h1> home page </h1>
+</Layout>  
+  )
+  }
