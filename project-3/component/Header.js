97777ccd011fcaea52cff52b1a2a5ad0245466d7
IbrahimNAlyami <Ibrahim.nashir.eam.js@tuwaiq.edu.sa>
2021-12-21 00:35:26 +0300

    updatecss

--- project-3/component/Header.js
+++ project-3/component/Header.js
@@ -1,40 +1,28 @@
 import {Nav, } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css' 
 import { NavLink } from 'react-bootstrap';
-import myHome from '../styles/Home.module.css'
+import myHeader from "../styles/Header.module.css"
 
 export default function Header() {
   return (
-    <div>
+    <div className={myHeader.header}>
         <Nav justify variant="tabs" defaultActiveKey="/home" >
-        <Nav.Item>
-    <Nav.Link className={myHome.hvrPush} href="/about">About  </Nav.Link>
+        <Nav.Item >
+    <Nav.Link className={myHeader.header} href="/about">About  </Nav.Link>
   </Nav.Item>
 
-  <Nav.Item>
-    <Nav.Link className={myHome.hvrPush} href="/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+  <Nav.Item >
+    <Nav.Link className={myHeader.hvrPush} href="/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
   <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
 </svg> </Nav.Link>
   </Nav.Item>
 
   <Nav.Item>
-    <Nav.Link className={myHome.hvrPush} href="/login">Login </Nav.Link>
+    <Nav.Link className={myHeader.hvrPush} href="/login">Login </Nav.Link>
   </Nav.Item>
  
 </Nav>
 
-
-      {/* <Link href="/">
-        <a style={MyStyle}>Home</a>
-      </Link>
-
-      <Link href="/about">
-        <a style={MyStyle}>About</a>
-      </Link>
-     
-      <Link href="/login">
-        <a style={MyStyle}>Login </a>
-      </Link> */}
     </div>
   )
 }
--- project-3/pages/index.js
+++ project-3/pages/index.js
@@ -1,53 +1,59 @@
 import Layout from "../component/MyLayout";
-import styles from "../styles/Home.module.css";
-import { Carousel } from "react-bootstrap";
+import { Carousel, Row, Col, Card, CardGroup } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import img from '../public/images/PNG/Drawkit-Vector-Illustration-Medical-01.png'
 import Image  from "next/image";
+import styles from "../styles/Home.module.css";
+
 
 export default function Home() {
   return (
     <Layout>
       
       <div className={styles.backgroundImage}>
-      <Carousel fade>
-  <Carousel.Item>
-    <Image
-      className="d-block w-70px"
-      src={img}
-      alt="First slide"
-      
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src=""
-      alt="Second slide"
-    />
+      <CardGroup>
+  <Card className={styles.rowOfCard}>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+      In the hospital, we strive to provide the finest medical services, looking forward to the highest 
+      medical and medical levels and attracting distinguished medical competencies with the latest medical 
+      devices, providing their medical services. All this, we put our eyes on the university and what our
+       leadership aspires to.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    </Card.Footer>
+  </Card>
+  <Card className={styles.rowOfCard}>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src=""
-      alt="Third slide"
-    />
+    </Card.Footer>
+  </Card>
+  <Card className={styles.rowOfCard}>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+    </Card.Footer>
+  </Card>
+</CardGroup>
       
       </div>
   
