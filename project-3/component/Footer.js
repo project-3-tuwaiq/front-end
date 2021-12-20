4f8ede2e8bb68ebecdcae29f18e9b7ea75e397bc
[batoolHma] <[batool.alsadi.eam.js@tuwaiq.edu.sa]>
2021-12-20 22:35:54 +0300

    updatefront

--- project-3/component/Footer.js
+++ project-3/component/Footer.js
@@ -1,8 +1,28 @@
+import { Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function Footer() {
-    return (
-        <div>
-            <h1>footer</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Row className="footer">
+        <Col xs={6} md={4}>
+          <img
+            className="footerimg"
+            src="https://cdn2.iconfinder.com/data/icons/medical-services-set-3-1/64/x-01-512.png"
+            width="150px"
+            
+          />
+         
+        </Col>
+        <Col> </Col>
+       
+      </Row>
+      <Row>
+<Col><img src=""/></Col>
+<Col></Col>
+
+
+      </Row>
+    </div>
+  );
 }
